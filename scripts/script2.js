1cfc078e5355b1bf023f89ac2a97c78fa5fb18cd
Phalente Robyn <phalenterobyn@gmail.com>
2023-04-26 11:06:15 +0200

    Script2.js cleanup

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,23 +1,3 @@
-const useNewGame = document.querySelector(".feature__newGame");
-const useFaceDice = document.querySelector(".feature__dice");
-const useRollDice = document.querySelector(".feature__rollDice");
-const useHold = document.querySelector(".feature__hold");
-
-const editBody = document.getElementsByTagName("body");
-const editContainer = document.querySelector(".container");
-const editPlayer1 = document.querySelector(".player1");
-const editPlayer2 = document.querySelector(".player2");
-const displayFaceDice = document.getElementById("img-dice");
-const displayPlayer1Name = document.querySelector(".player__userName1-h2");
-const displayPlayer2Name = document.querySelector(".player__userName2-h2");
-const player1GlobalScore = document.querySelector(".player__globalScore1-p");
-const player2GlobalScore = document.querySelector(".player__globalScore2-p");
-const player1CurrentScore = document.querySelector(
-  ".player__current__score1-p"
-);
-const player2CurrentScore = document.querySelector(
-  ".player__current__score2-p"
-);
 class Player {
   constructor(name, globalScore, currentScore) {
     this.name = name;
@@ -49,6 +29,80 @@ class Player {
   }
 }
 
+const useNewGame = document.querySelector(".feature__newGame");
+const useFaceDice = document.querySelector(".feature__dice");
+const useRollDice = document.querySelector(".feature__rollDice");
+const useHold = document.querySelector(".feature__hold");
+
+const editBody = document.getElementsByTagName("body");
+const editContainer = document.querySelector(".container");
+const editPlayer1 = document.querySelector(".player1");
+const editPlayer2 = document.querySelector(".player2");
+const displayFaceDice = document.getElementById("img-dice");
+const displayPlayer1Name = document.querySelector(".player__userName1-h2");
+const displayPlayer2Name = document.querySelector(".player__userName2-h2");
+const player1GlobalScore = document.querySelector(".player__globalScore1-p");
+const player2GlobalScore = document.querySelector(".player__globalScore2-p");
+const player1CurrentScore = document.querySelector(
+  ".player__current__score1-p"
+);
+const player2CurrentScore = document.querySelector(
+  ".player__current__score2-p"
+);
+
+let player1 = new Player("Jb", 0, 0);
+let player2 = new Player("Charles", 0, 0);
+let currentPlayer = player1;
+let diceScore = "";
+let victory = false;
+let winner = "";
+
+changeBackground();
+// changeBackgroundGameWon();
+
+useNewGame.addEventListener("click", newGame);
+useHold.addEventListener("click", funcHold);
+useRollDice.addEventListener("click", throwtheDice);
+useFaceDice.addEventListener("click", throwtheDice);
+
+function changeBackground() {
+  if (currentPlayer == player1 && winner == "") {
+    document.body.style.backgroundImage =
+      "linear-gradient(90deg, var(--active-color-1) 50%, var(--active-color-2) 50%)";
+    displayPlayer1Name.style.fontWeight = "300";
+    displayPlayer2Name.style.fontWeight = "200";
+  } else if (currentPlayer == player2 && winner == "") {
+    document.body.style.backgroundImage =
+      "linear-gradient(90deg, var(--active-color-2) 50%, var(--active-color-1) 50%)";
+    displayPlayer2Name.style.fontWeight = "300";
+    displayPlayer1Name.style.fontWeight = "200";
+  } else if (currentPlayer == player1 && winner == player1) {
+    document.body.style.backgroundImage =
+      "linear-gradient(90deg, var(--color-win) 50%, (--color-lose) 50%);";
+  } else if (currentPlayer == player2 && winner == player2) {
+    document.body.style.backgroundImage =
+      "linear-gradient(90deg, (--color-lose) 50%, var(--color-win) 50%);";
+  }
+}
+
+// function changeBackgroundGameWon(){
+//     if (currentPlayer == player1 && winner == player1) {
+//         document.body.style.backgroundImage = 'linear-gradient(90deg, var(--color-win) 50%, (--color-lose) 50%);'
+//     }else if (currentPlayer == player2 && winner == player2) {
+//         document.body.style.backgroundImage = 'linear-gradient(90deg, (--color-lose) 50%, var(--color-win) 50%);'
+//     }
+// };
+
+function changeFaceDice() {
+  displayFaceDice.src = `./images/dice-${diceScore}.svg`;
+  displayFaceDice.setAttribute("alt", `Dé numéro ${diceScore}`);
+}
+function randomDice() {
+  diceScore = Math.floor(Math.random() * 6 + 1);
+  changeFaceDice();
+  console.log(`dice = ${diceScore}`);
+}
+
 function newGame() {
   player1.initPlayer(player1.name, 0, 0);
   player2.initPlayer(player2.name, 0, 0);
@@ -63,85 +117,10 @@ function newGame() {
   useFaceDice.addEventListener("click", throwtheDice);
 }
 
-function changeBackground() {
-  if (currentPlayer == player1) {
-    document.body.style.backgroundImage =
-      "linear-gradient(90deg, var(--active-color-1) 50%, var(--active-color-2) 50%)";
-    displayPlayer1Name.style.fontWeight = "300";
-    displayPlayer2Name.style.fontWeight = "200";
-  } else if (currentPlayer == player2) {
-    document.body.style.backgroundImage =
-      "linear-gradient(90deg, var(--active-color-2) 50%, var(--active-color-1) 50%)";
-    displayPlayer2Name.style.fontWeight = "300";
-    displayPlayer1Name.style.fontWeight = "200";
-  }
-}
-
-function changeBackgroundGameWon() {
-  if ((winner = player1)) {
-    document.body.style.backgroundImage =
-      "linear-gradient(90deg, var(--color-win) 50%, (--color-lose) 50%);";
-  } else if ((winner = player2)) {
-    document.body.style.backgroundImage =
-      "linear-gradient(90deg, (--color-lose) 50%, var(--color-win) 50%);";
-  }
-}
-
 function disableDiceAndHold() {
-  useFaceDice.removeEventListener("click", randomDice);
-  useRollDice.removeEventListener("click", randomDice);
-  useHold.removeEventListener("click", hold);
-}
-
-function changeFaceDice() {
-  displayFaceDice.src = `./images/dice-${diceScore}.svg`;
-  displayFaceDice.setAttribute("alt", `Dé numéro ${diceScore}`);
-}
-function randomDice() {
-  diceScore = Math.floor(Math.random() * 6 + 1);
-  changeFaceDice();
-  console.log(`dice = ${diceScore}`);
-}
-
-function funcHold() {
-  console.log(
-    `currentPlayer : ${currentPlayer.name}, player1 : ${player1.name}, player2 : ${player2.name}`
-  );
-  if (currentPlayer == player1) {
-    player1.hold();
-    player1GlobalScore.textContent = `${player1.globalScore}`;
-    player1CurrentScore.textContent = `${player1.currentScore}`;
-    if (player1.gameWon() === true) {
-      winner = player1;
-      useHold.removeEventListener("click", funcHold);
-      useRollDice.removeEventListener("click", throwtheDice);
-      useFaceDice.removeEventListener("click", throwtheDice);
-      alert(`${player1.name} à gagné`);
-      changeBackgroundGameWon();
-    }
-    currentPlayer = player2;
-    changeBackground();
-    console.log(
-      `Hold : \n currentPlayer : ${currentPlayer.name}, player1 : ${player1.name}, player2 : ${player2.name}`
-    );
-  } else {
-    player2.hold();
-    player2GlobalScore.textContent = `${player2.globalScore}`;
-    player2CurrentScore.textContent = `${player2.currentScore}`;
-    if (player2.gameWon() === true) {
-      winner = player2;
-      useHold.removeEventListener("click", funcHold);
-      useFaceDice.removeEventListener("click", throwtheDice);
-      useFaceDice.removeEventListener("click", throwtheDice);
-      alert(`${player2.name} à gagné`);
-      changeBackgroundGameWon();
-    }
-    currentPlayer = player1;
-    changeBackground();
-    console.log(
-      `Hold : \n currentPlayer : ${currentPlayer.name}, player1 : ${player1.name}, player2 : ${player2.name}`
-    );
-  }
+  useHold.removeEventListener("click", funcHold);
+  useRollDice.removeEventListener("click", throwtheDice);
+  useFaceDice.removeEventListener("click", throwtheDice);
 }
 
 function throwtheDice() {
@@ -174,79 +153,41 @@ function throwtheDice() {
   }
 }
 
-let player1 = new Player("Jb", 0, 0);
-let player2 = new Player("Charles", 0, 0);
-let currentPlayer = player1;
-let diceScore = "";
-let victory = false;
-let winner = "";
-
-useNewGame.addEventListener("click", newGame);
-
-changeBackground();
-changeBackgroundGameWon();
-
-// [useRollDice, useFaceDice].forEach(item => {
-//     item.addEventListener('click', event => {
-//         console.log(currentPlayer);
-//         randomDice();
-//         if (currentPlayer == player1) {
-//             player1.current();
-//             player1CurrentScore.textContent = `${player1.currentScore}`;
-//             console.log(`currentScore de ${currentPlayer.name} = ${player1.currentScore}`);
-//             if (diceScore == 1){
-//                 player1.currentScore = 0;
-//                 currentPlayer = player2;
-//                 changeBackground();
-//                 console.log(currentPlayer);
-//             }
-//         } else {
-//             player2.current();
-//             player2CurrentScore.textContent = `${player2.currentScore}`;
-//             console.log(`currentScore de ${currentPlayer.name} = ${currentPlayer.currentScore}`);
-//             if (diceScore == 1){
-//                 player2.currentScore = 0;
-//                 currentPlayer = player1;
-//                 changeBackground();
-//                 console.log(currentPlayer);
-//             }
-//         }
-//     });
-// });
-
-// useHold.addEventListener('click', eventhold => {
-//     console.log(`currentPlayer : ${currentPlayer.name}, player1 : ${player1.name}, player2 : ${player2.name}`);
-//     if (currentPlayer == player1) {
-//         player1.hold();
-//         player1GlobalScore.textContent = `${player1.globalScore}`;
-//         player1CurrentScore.textContent = `${player1.currentScore}`;
-//         if (player1.gameWon() === true) {
-//             winner = player1;
-//             useHold.removeEventListener('click', eventhold);
-//             alert(`${player1.name} à gagné`);
-//             changeBackgroundGameWon();
-//         };
-//         currentPlayer = player2;
-//         changeBackground();
-//         console.log(`Hold : \n currentPlayer : ${currentPlayer.name}, player1 : ${player1.name}, player2 : ${player2.name}`);
-
-//     }else {
-//         player2.hold();
-//         player2GlobalScore.textContent = `${player2.globalScore}`;
-//         player2CurrentScore.textContent = `${player2.currentScore}`;
-//         if (player2.gameWon() === true) {
-//             winner = player2;
-//             useHold.removeEventListener('click', eventhold);
-//             alert(`${player2.name} à gagné`);
-//             changeBackgroundGameWon();
-//         };
-//         currentPlayer = player1;
-//         changeBackground();
-//         console.log(`Hold : \n currentPlayer : ${currentPlayer.name}, player1 : ${player1.name}, player2 : ${player2.name}`);
-
-//     }
-// });
-
-useHold.addEventListener("click", funcHold);
-useRollDice.addEventListener("click", throwtheDice);
-useFaceDice.addEventListener("click", throwtheDice);
+function funcHold() {
+  console.log(
+    `currentPlayer : ${currentPlayer.name}, player1 : ${player1.name}, player2 : ${player2.name}`
+  );
+  if (currentPlayer == player1) {
+    player1.hold();
+    player1GlobalScore.textContent = `${player1.globalScore}`;
+    player1CurrentScore.textContent = `${player1.currentScore}`;
+    if (player1.gameWon() === true) {
+      winner = player1;
+      currentPlayer = player1;
+      disableDiceAndHold();
+      alert(`${player1.name} à gagné`);
+      changeBackground();
+    }
+    currentPlayer = player2;
+    changeBackground();
+    console.log(
+      `Hold : \n currentPlayer : ${currentPlayer.name}, player1 : ${player1.name}, player2 : ${player2.name}`
+    );
+  } else {
+    player2.hold();
+    player2GlobalScore.textContent = `${player2.globalScore}`;
+    player2CurrentScore.textContent = `${player2.currentScore}`;
+    if (player2.gameWon() === true) {
+      winner = player2;
+      currentPlayer = player2;
+      disableDiceAndHold();
+      alert(`${player2.name} à gagné`);
+      changeBackground();
+    }
+    currentPlayer = player1;
+    changeBackground();
+    console.log(
+      `Hold : \n currentPlayer : ${currentPlayer.name}, player1 : ${player1.name}, player2 : ${player2.name}`
+    );
+  }
+}
