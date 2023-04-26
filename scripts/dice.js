1151a3c80804cb9b7059f12fd84bdebef876e91e
Phalente Robyn <phalenterobyn@gmail.com>
2023-04-26 10:33:43 +0200

    Change the Dice face depending on the result of randomDice()

--- scripts/dice.js
+++ scripts/dice.js
@@ -1,40 +1,23 @@
-import * as useQuery from "./useQuery";
-
-function randomDice(nbMaxDice) {
-  let diceScore = Math.floor(Math.random() * nbMaxDice + 1);
-  console.log(`dice = ${diceScore}`);
-}
-
-function throwTheDice() {
-  useQuery.useRollDice.addEventListener("click", (e) => {
-    randomDice(6);
-    changeFaceDice();
-  });
-  useDice.addEventListener("click", (e) => {
-    randomDice(6);
-    changeFaceDice();
-  });
-}
+// import * as useQuery  from './useQuery.js';
+const useRollDice = document.querySelector(".feature__rollDice");
+const displayFaceDice = document.getElementById("img-dice");
+const useFaceDice = document.querySelector(".feature__dice");
+let diceScore = "";
 
 function changeFaceDice() {
-  if (diceScore == 1) {
-    displayFaceDice.src = "../images/dice-1.svg";
-  } else if (diceScore == 2) {
-    displayFaceDice.src = "../images/dice-2.svg";
-  } else if (diceScore == 3) {
-    displayFaceDice.src = "../images/dice-3.svg";
-  } else if (diceScore == 4) {
-    displayFaceDice.src = "../images/dice-4.svg";
-  } else if (diceScore == 5) {
-    displayFaceDice.src = "../images/dice-5.svg";
-  } else if (diceScore == 6) {
-    displayFaceDice.src = "../images/dice-6.svg";
-  }
+  displayFaceDice.src = `./images/dice-${diceScore}.svg`;
 }
 
-// function changeFaceDice(diceScore){
-//     displayFaceDice.src = `../images/dice-${diceScore}.svg`;
-// }
+function randomDice() {
+  diceScore = Math.floor(Math.random() * 6 + 1);
+  console.log(`dice = ${diceScore}`);
+  displayFaceDice.src = `./images/dice-${diceScore}.svg`;
+}
 
-throwTheDice();
-console.error(error);
+useRollDice.addEventListener("click", (throwTheDice) => {
+  randomDice();
+});
+
+useFaceDice.addEventListener("click", (throwTheDice) => {
+  randomDice();
+});
