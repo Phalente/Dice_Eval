80f2ad13e730941c17c458fd344b7884dcc716c7
Phalente Robyn <phalenterobyn@gmail.com>
2023-04-26 10:51:14 +0200

    Testing a new algorithm

--- scripts/script2.js
+++ scripts/script2.js
@@ -2,7 +2,6 @@ const useNewGame = document.querySelector(".feature__newGame");
 const useFaceDice = document.querySelector(".feature__dice");
 const useRollDice = document.querySelector(".feature__rollDice");
 const useHold = document.querySelector(".feature__hold");
-
 const dom = document.getElementsByTagName("body");
 const editPlayer1 = document.querySelector(".player1");
 const editPlayer2 = document.querySelector(".player2");
@@ -17,15 +16,14 @@ const player1CurrentScore = document.querySelector(
 const player2CurrentScore = document.querySelector(
   ".player__current__score2-p"
 );
-
 function changeFaceDice() {
   displayFaceDice.src = `./images/dice-${diceScore}.svg`;
 }
 
 function randomDice() {
   diceScore = Math.floor(Math.random() * 6 + 1);
+  changeFaceDice();
   console.log(`dice = ${diceScore}`);
-  displayFaceDice.src = `./images/dice-${diceScore}.svg`;
 }
 
 class Player {
@@ -43,11 +41,9 @@ class Player {
   current() {
     if (diceScore !== 1) {
       this.currentScore += diceScore;
-    } else if ((currentPlayer = player1)) {
+    } else {
       this.currentScore = 0;
-      currentPlayer = player2;
     }
-    console.log(`currentScore de ${this.name} = ${this.currentScore}`);
   }
 
   hold() {
@@ -55,28 +51,66 @@ class Player {
     this.currentScore = 0;
   }
 }
-
 let player1 = new Player("Jb", 0, 0);
 let player2 = new Player("Charles", 0, 0);
 let currentPlayer = player1;
 let diceScore = "";
-
 displayPlayer1Name.textContent = `${player1.name}`;
 player1GlobalScore.textContent = `${player1.currentScore}`;
 displayPlayer2Name.textContent = `${player2.name}`;
-player2GlobalScore.textContent = `${player2.currentScore}`;
+player2CurrentScore.textContent = `${player2.currentScore}`;
 // player1.current(randomDice);
 
 // if currentPlayer = player1 on modifie
+// [useRollDice, useFaceDice].forEach(item => {
+//     item.addEventListener('click', event => {
+//         randomDice();
+//         if (currentPlayer = player1){
+//             player1.current();
+//             player1CurrentScore.textContent = `${player1.currentScore}`;
+//             console.log(`currentScore de ${player1.name} = ${player1.currentScore}`);
+//             if (diceScore == 1){
+//                 currentPlayer = player2;
+//                 console.log(currentPlayer);
+//                 return currentPlayer = player2;
+//             }
+//         } else if (currentPlayer = player2) {
+//             player2.current();
+//             player2CurrentScore.textContent = `${player2.currentScore}`;
+//             console.log(`currentScore de ${player2.name} = ${player2.currentScore}`);
+//             if (diceScore == 1){
+//                 currentPlayer = player1;
+//                 console.log(currentPlayer);
+//         }
+//     }
+// })
+// });
+
 [useRollDice, useFaceDice].forEach((item) => {
   item.addEventListener("click", (event) => {
     randomDice();
     if ((currentPlayer = player1)) {
-      player1.current();
-      player1CurrentScore.textContent = `${player1.currentScore}`;
-    } else {
+      currentPlayer.current();
+      player1CurrentScore.textContent = `${currentPlayer.currentScore}`;
+      console.log(
+        `currentScore de ${currentPlayer.name} = ${currentPlayer.currentScore}`
+      );
+      if (diceScore == 1) {
+        currentPlayer == player2;
+      }
+    } else if ((currentPlayer = player2)) {
       player2.current();
       player2CurrentScore.textContent = `${player2.currentScore}`;
+      console.log(`currentScore de ${player2.name} = ${player2.currentScore}`);
+      if (diceScore == 1) {
+        currentPlayer = player1;
+        console.log(currentPlayer);
+      }
     }
   });
+});
+
+useHold.addEventListener("click", (event) => {
+  currentPlayer.hold();
+  player1GlobalScore.textContent = `${player1.globalScore}`;
 });
